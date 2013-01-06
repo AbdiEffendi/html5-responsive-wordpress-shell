5511b484d6b282b0a4671de0b2b658161c78b5cc
Brent Lagerman <brent@mimoymima.com>
2013-01-06 11:41:54 -0500

    adding code that fixes a resizing issue when changing orientation on old ipad/ipone

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,21 @@
 // last edited: Nov 27, 2011
 
 
+// fix for ipad resizing content on orientation change by Jeremy Keith
+// useful for making responsive sites, if your site isn't responsive you can remove this bit of code
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+var viewportmeta = document.querySelector('meta[name="viewport"]');
+if (viewportmeta) {
+    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+    document.body.addEventListener('gesturestart', function () {
+        viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    }, false);
+	}
+}
+
+
 // DOCUMENT READY FUNCTION: uses noConflict to work with other libraries
-jQuery(document).ready(function($) {	
+jQuery(document).ready(function($) {
 
 //  // site preloader -- also uncomment the div in the header and the css style for #preloader
 //  $(window).load(function(){
